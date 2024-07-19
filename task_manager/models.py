a047c8ee708d93166ef099df402173e4d8ab218a
vamsiboppe99 <165534337+vamsiboppe99@users.noreply.github.com>
2024-07-19 16:55:51 -0500

    Add files via upload

---
+++ task_manager/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return self.title
